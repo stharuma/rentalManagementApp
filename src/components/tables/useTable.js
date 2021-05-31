e1a6f971d67dcac7f9952abcdcd011c4f39f9603
stharuma <stharuma@gmail.com>
2021-05-31 18:23:27 -0400

    Added sorting functionality to custom table

--- src/components/tables/useTable.js
+++ src/components/tables/useTable.js
@@ -1,45 +1,71 @@
 import React, { useState } from "react";
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-} from "@material-ui/core";
+import { Table, TableHead, TableRow, TableCell, TableSortLabel } from "@material-ui/core";
 import useStyles from "./useTableStyle";
 import UsePagination from "./usePagination";
+import UseSort from "./useSort";
 
 /**
-* @author Suresh Tharuma
-* @function UseTable
-**/
+ * @author Suresh Tharuma
+ * @function UseTable
+ **/
 
 const UseTable = (headCells, records) => {
-    const classes = useStyles();
-    const { TblPagination, recordsAfterPaging } = UsePagination(records);
+  const classes = useStyles();
+  const [order, setOrder] = useState();
+  const [orderBy, setOrderBy] = useState();
 
-    const TblContainer = (props) => (
-      <Table className={classes.table}>{props.children}</Table>
+  const { TblPagination, recordsAfterPaging } = UsePagination(records);
+  const { stableSort, getComparator } = UseSort();
+
+  const dataAfterPagingAndSorting =() => recordsAfterPaging(
+    stableSort(records, getComparator(order, orderBy))
+  );
+
+  const TblContainer = (props) => (
+    <Table className={classes.table}>{props.children}</Table>
+  );
+
+  const TblHead = (props) => {
+
+    const handleSort = (cellId) => {
+      const isAsc = orderBy === cellId && order === "asc";
+      setOrder(isAsc ? "desc" : "asc");
+      setOrderBy(cellId);
+    };
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              {headCell.disableSorting ? (
+                headCell.label
+              ) : (
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={() => {
+                    handleSort(headCell.id);
+                  }}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              )}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
     );
-  
-    const TblHead = (props) => {
-      return (
-        <TableHead>
-          <TableRow>
-            {headCells.map((headCell) => (
-              <TableCell key={headCell.id}>{headCell.label}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-      );
-    };
-  
-    return {
-      TblContainer,
-      TblHead,
-      TblPagination, 
-      recordsAfterPaging
-    };
   };
-  
-  export default UseTable;
-  
+
+  return {
+    TblContainer,
+    TblHead,
+    TblPagination,
+    dataAfterPagingAndSorting,
+  };
+};
+
+export default UseTable;
