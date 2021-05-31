437286593ff551cd1483c9e1e7f171c90e0f9a17
stharuma <stharuma@gmail.com>
2021-05-31 12:33:17 -0400

    Added rentalProduct page

---
+++ src/components/pages/rentalProducts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+* @author Suresh Tharuma
+* @function RentalProducts
+**/
+
+const RentalProducts = (props) => {
+  return(
+    <div>RentalProducts</div>
+   )
+
+ }
+
+export default RentalProducts
