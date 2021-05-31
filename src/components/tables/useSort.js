8dae4e1824f9794f76a086077c8d50aeaa934e7f
stharuma <stharuma@gmail.com>
2021-05-31 18:21:23 -0400

    added sorting component

---
+++ src/components/tables/useSort.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+/**
+ * @author Suresh Tharuma
+ * @function UseSort
+ **/
+
+const UseSort = (props) => {
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
+  const getComparator = (order, orderBy) =>
+    order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+  return {
+    stableSort,
+    getComparator,
+  };
+};
+
+export default UseSort;
