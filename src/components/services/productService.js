08001372aec085378140be17ad60694c0cfb925f
stharuma <stharuma@gmail.com>
2021-05-31 12:34:13 -0400

    Added productService component

---
+++ src/components/services/productService.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+* @author Suresh Tharuma
+* @function ProductService
+**/
+
+const ProductService = (props) => {
+  return(
+    <div>ProductService</div>
+   )
+
+ }
+
+export default ProductService
