fe0edd2c5b4b214c4454f7d3692c722fea6d4af0
stharuma <stharuma@gmail.com>
2021-05-31 23:09:41 -0400

    Updated based on new custom fields

--- src/components/common/Controls.js
+++ src/components/common/Controls.js
@@ -1,7 +1,13 @@
 import Input from "./Input";
+import Button from "./Button";
+import ActionButton from "./ActionButton";
+import Popup from "./Popup";
 
 const Controls = {
     Input,
+    Button,
+    ActionButton,
+    Popup
 }
 
 export default Controls;
