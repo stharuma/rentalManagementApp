adf1a36359d44ca7d5529c4fd742cb4f11485339
stharuma <stharuma@gmail.com>
2021-06-02 08:22:52 -0400

    updated selectProduct form

--- src/components/pages/products/forms/selectProductForm.js
+++ src/components/pages/products/forms/selectProductForm.js
@@ -10,27 +10,31 @@ import * as productService from "../../../services/productService";
  **/
 const useStyles = makeStyles((theme) => ({
   pageContent: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(1),
-    flexGrow: 1,
+    // margin: theme.spacing(1),
+    // padding: theme.spacing(1),
+    // flexGrow: 1,
   },
 }));
 
 const SelectedProduct = (props) => {
   const classes = useStyles();
-  const {isBook, bookedProduct, setBookedProduct, returnProduct, setReturnProduct } = props;
+  const {
+    isBook,
+    bookedProduct,
+    setBookedProduct,
+    returnProduct,
+    setReturnProduct,
+  } = props;
   const [availableProductsByName, setAvailableProductsByName] = useState([]);
-  const [notAvailableProductsByName, setNotAvailableProductsByName] = useState([]);
+  const [rentedProductsByName, setRentedProductsByName] = useState([]);
 
   const _onInputNameChange = (event, value) => {
     console.log("value ", value);
-    if(isBook){
+    if (isBook) {
       setAvailableProductsByName(productService.getAllAvailableProducts());
-    }else{
-      setNotAvailableProductsByName(productService.getAllNotAvailableProducts());
+    } else {
+      setRentedProductsByName(productService.getAllRentedProducts());
     }
-      
-    
   };
 
   const onFocus = (event) => {
@@ -41,27 +45,33 @@ const SelectedProduct = (props) => {
 
   return (
     <>
-      <Grid container spacing={3} className={classes.pageContent}>
+      <Grid container spacing={1} className={classes.pageContent}>
         <Grid item xs={12} sm={4}>
           <Autocomplete
             id="fname"
             name="name"
             value={isBook ? bookedProduct : returnProduct}
-            options={isBook ? availableProductsByName : notAvailableProductsByName}
-            getOptionLabel={(option) => option && option.name ? option.name + "/" + option.code :""}
+            options={isBook ? availableProductsByName : rentedProductsByName}
+            getOptionLabel={(option) =>
+              option && option.name ? option.name + "/" + option.code : ""
+            }
             style={{ width: 500 }}
             onInputChange={_onInputNameChange}
             onChange={(e, value) => {
-              isBook ? setBookedProduct(value):setReturnProduct(value);
+              isBook ? setBookedProduct(value) : setReturnProduct(value);
             }}
             renderInput={(params) => (
               <Controls.Input
                 {...params}
-                label={isBook ? "Select Product For Book" : "Select Product For Return"}
+                label={
+                  isBook
+                    ? "Select Product For Book"
+                    : "Select Product For Return"
+                }
                 variant="outlined"
                 fullWidth
                 autoComplete="off"
-                              />
+              />
             )}
           />
         </Grid>
