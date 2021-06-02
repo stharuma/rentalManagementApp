27df2be8916888429823b460fbb62a0563a8d159
stharuma <stharuma@gmail.com>
2021-06-02 08:13:44 -0400

    Export DatePicker

--- src/components/common/Controls.js
+++ src/components/common/Controls.js
@@ -2,12 +2,15 @@ import Input from "./Input";
 import Button from "./Button";
 import ActionButton from "./ActionButton";
 import Popup from "./Popup";
+import DatePicker from "./DatePicker"
 
 const Controls = {
     Input,
     Button,
     ActionButton,
-    Popup
+    Popup,
+    DatePicker
+
 }
 
 export default Controls;
